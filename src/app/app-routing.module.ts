d1e0e3cb1437b5848f4eb6bb4fe001a063c76a94
Laurence Leysen TM <laurenceleysen5@gmail.com>
2024-06-02 14:42:33 +0200

    messed up educationcomponent route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'skills', component: SkillsComponent },
-  { path: 'education', component: SkillsComponent },
+  { path: 'education', component: EducationComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent }
 ];
